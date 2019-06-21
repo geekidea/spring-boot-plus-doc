786e6b0ac3501244ff8469b17bec3315fa5c0948
geekidea <geekidea@qq.com>
2019-06-21 17:45:21 +0800

    spring-boot-plus-doc

---
+++ .vuepress/config.js
@@ -0,0 +1,99 @@
+module.exports = {
+  port: "3000",
+  dest: "docs",
+  ga: "UA-85414008-1",
+  base: "/",
+  markdown: {
+    externalLinks: {
+      target: '_blank', rel: 'noopener noreferrer'
+    }
+  },
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "spring-boot-plus",
+      description: "spring boot快速开发脚手架"
+    }
+  },
+  head: [["link", { rel: "icon", href: `/favicon.ico` }]],
+  themeConfig: {
+    repo: "geekidea/spring-boot-plus",
+    docsRepo: "springboot-plus/springboot-plus.github.io",
+    editLinks: true,
+    locales: {
+      "/": {
+        label: "简体中文",
+        selectText: "Languages",
+        editLinkText: "在 GitHub 上编辑此页",
+        lastUpdated: "上次更新",
+        nav: [
+          {
+            text: "指南",
+            link: "/guide/"
+          },
+          {
+            text: "配置",
+            link: "/config/"
+          },
+          {
+            text: "生态",
+            items: [
+              {
+                text: "spring-boot-assembly",
+                link: "https://gitee.com/geekidea/spring-boot-assembly"
+              }
+            ]
+          },
+          {
+            text: "更新日志",
+            link:
+              ""
+          }
+        ],
+        sidebar: {
+          "/guide/": genGuideSidebar(true),
+          "/config/": genConfigSidebar(true)
+        }
+      }
+    }
+  }
+};
+
+function genGuideSidebar() {
+  return [
+    {
+      title: "快速入门",
+      collapsable: false,
+      children: ["", "quick-start"]
+    },
+    {
+      title: "核心功能" ,
+      collapsable: false,
+      children: ["core"]
+    },
+    {
+      title: "插件扩展",
+      collapsable: false,
+      children: [
+        "quick-start"
+      ]
+    },
+    {
+      title: "FAQ",
+      collapsable: false,
+      children: [
+        "faq"
+      ]
+    }
+  ]
+}
+
+function genConfigSidebar() {
+  return [
+    {
+      title: "配置" ,
+      collapsable: false,
+      children: ["", "generator-config"]
+    }
+  ]
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# spring-boot-plus-doc
-spring-boot-plus-doc
+---
+home: true
+heroImage: /img/logo.png
+actionText: 快速开始 →
+actionLink: /guide/
+features:
+- title: 快速开发
+  details: 
+- title: 高效易用
+  details: 
+- title: 功能丰富
+  details: 
+footer: Apache License 2.0 | © 2019-2029 geekidea
+---
+
+
