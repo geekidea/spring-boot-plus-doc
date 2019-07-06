a5978f67b0703d0266aa68448f2beecfacfbeb4a
geekidea <geekidea@qq.com>
2019-07-06 12:10:30 +0800

    quick start

--- .vuepress/theme/Home.vue
+++ .vuepress/theme/Home.vue
@@ -13,19 +13,13 @@
         {{ data.tagline || $description || 'Welcome to your VuePress site' }}
       </p>
 
-<!--      <p-->
-<!--        class="action"-->
-<!--        v-if="data.actionText && data.actionLink"-->
-<!--      >-->
-<!--        <NavLink-->
-<!--          class="action-button"-->
-<!--          :item="actionLink"-->
-<!--        />-->
-<!--      </p>-->
-
       <p>
-        <a href="/guide/" class="nav-link action-button">快速开始 →</a>
-        <a href="https://github.com/geekidea/spring-boot-plus" class="nav-link action-button go-github-btn" TARGET="_blank">GITHUB →</a>
+        <a href="/guide/" class="nav-link action-button quick-btn">快速开始 →</a>
+        <a href="https://github.com/geekidea/spring-boot-plus" class="nav-link action-button go-github-btn" TARGET="_blank">
+          <svg aria-labelledby="simpleicons-github-dark-icon" lang="" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title id="simpleicons-github-icon" lang="en">GitHub Dark icon</title><path fill="#7F8C8D" d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path></svg>
+
+
+          GITHUB</a>
       </p>
 
     </div>
@@ -85,8 +79,6 @@ export default {
   margin 0px auto
   .hero
     text-align center
-    .go-github-btn
-
     img
       max-width 100%
       max-height 280px
@@ -170,5 +162,32 @@ export default {
         font-size 1.25rem
 
 
+
+</style>
+
+<style>
+  .quick-btn{
+    border-radius: 60px!important;
+    padding: 10px 30px 10px 30px !important;
+  }
+  .go-github-btn{
+    margin-left: 20px!important;
+    border-radius: 60px!important;
+    position: relative;
+    padding: 10px 30px 12px 60px !important;
+    background-color: #f6f6f6!important;
+    color: #4f5959!important;
+    border-color: #f6f6f6!important;
+    font-size: 1.05em!important;
+    font-weight: 600!important;
+    letter-spacing: 0.1em!important;
+  }
+  .go-github-btn svg{
+    position: absolute;
+    left: 0.4em;
+    top: 0.4em;
+    width: 2em;
+    padding-left: 6px;
+  }
 
 </style>
--- guide/README.md
+++ guide/README.md
@@ -1,1 +1,52 @@
 # 简介
+
+### spring-boot-plus是一套集成spring boot常用开发组件的后台快速开发框架
+
+#### 官网地址：[springboot.plus](http://springboot.plus "springboot.plus")
+#### GITHUB：[https://github.com/geekidea/spring-boot-plus](https://github.com/geekidea/spring-boot-plus "spring-boot-plus sources")
+
+博客：[geekidea.io](https://geekidea.io/ "geekidea.io")
+
+### 主要特性
+1. 集成spring boot 常用开发组件集
+2. 集成mybatis plus快速dao操作
+3. 快速生成后台代码: entity/param/vo/controller/service/mapper/xml
+4. 不同环境打包配置部署
+5. spring boot 公共配置、AOP日志等
+
+                
+- spring boot 2.1.6        
+- mybatis plus             
+- druid                    
+- mysql                    
+- redis                    
+- rabbit/rocket/kafka mq   
+- jwt + security           
+
+### 项目环境 
+中间件 | 版本 |  备注
+-|-|-
+JDK | 1.8+ | JDK1.8及以上 |
+MySQL | 5.7+ | 5.7及以上 |
+Redis | 3.2+ |  |
+
+### 技术选型 
+技术 | 版本 |  备注
+-|-|-
+Spring Boot | 2.1.6.RELEASE | 最新发布稳定版 |
+Spring Framework | 5.1.8.RELEASE | 最新发布稳定版 |
+Mybatis | 3.5.1 | 持久层框架 |
+Mybatis Plus | 3.1.1 | mybatis增强框架 |
+Alibaba Druid | 1.1.17 | 数据源 |
+Fastjson | 1.2.58 | JSON处理工具集 |
+swagger2 | 2.7.0 | api文档生成工具 |
+commons-lang3 | 3.9 | 常用工具包 |
+commons-io | 2.6 | IO工具包 |
+commons-codec | 1.12 | 加密解密等工具包 |
+commons-collections | 3.2.1 | 集合工具包 |
+reflections | 0.9.11 | 反射工具包 |
+hibernate-validator | 6.0.17.Final | 后台参数校验注解 |
+jwt | 0.9.1 | json web token |
+hutool-all | 4.5.10 | 常用工具集 |
+
+
